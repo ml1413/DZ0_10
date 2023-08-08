ac4a01e51b7ee8c69a0ee342cfdc5ccb49fbd3c1
ml1413 <ml1413sam@gmail.com>
2023-08-08 22:58:01 +0300

    version 2

--- app/src/main/java/com/example/dz0_10/MainActivity.kt
+++ app/src/main/java/com/example/dz0_10/MainActivity.kt
@@ -2,30 +2,71 @@ package com.example.dz0_10
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.TextView
 import android.widget.Toast
 
 
-class MainActivity : Activity() {
-    private var result = 2
+class MainActivity : Activity(), OnClickListener {
+    private var result = (1..10).random()
+    private lateinit var resultOnScreen: TextView
+    private lateinit var tvPlus: TextView
+    private lateinit var tvMinus: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
 
-        findViewById<TextView>(R.id.tv_plus).setOnClickListener {
-            showResultOnToast(+1)
-        }
-        findViewById<TextView>(R.id.tv_minus).setOnClickListener {
-            showResultOnToast(-1)
+    private fun init() {
+        resultOnScreen = findViewById(R.id.tv_result)
+        resultOnScreen.text = result.toString()
+
+        tvPlus = findViewById(R.id.tv_plus)
+        tvPlus.setOnClickListener(this)
+
+        tvMinus = findViewById(R.id.tv_minus)
+        tvMinus.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.tv_plus -> {
+                setValueOnResult(+1)
+                tvPlus.setAmin(R.anim.button_animatiom)
+            }
+
+            R.id.tv_minus -> {
+                setValueOnResult(-1)
+                tvMinus.setAmin(R.anim.button_animatiom)
+            }
         }
     }
 
-    private fun showResultOnToast(value: Int) {
-        if (result > 1 || value > 0) {
+    private fun setValueOnResult(value: Int) {
+        if ((result > 1 || value > 0) && (result < 10 || value < 0)) {
             result += value
-            Toast.makeText(this, result.toString(), Toast.LENGTH_SHORT).show()
+            resultOnScreen.text = result.toString()
+            resultOnScreen.setAmin(R.anim.myscale)
         } else {
-            Toast.makeText(this, "число 1 минимальное   $result", Toast.LENGTH_SHORT).show()
+            val message = if (result < 5) "1 минимальное число" else "10 максимальное число"
+            resultOnScreen.setAmin(R.anim.left_right)
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
         }
+    }
+
+    private fun TextView.setAmin(animation: Int) {
+        this.startAnimation(
+            AnimationUtils.loadAnimation(
+                this@MainActivity,
+                animation
+            )
+        )
     }
 }
